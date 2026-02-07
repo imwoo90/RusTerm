8b322211856df120b748dd09fab998f11c4906ed
imwoo90 <wim90@kakao.com>
2026-02-07 17:03:57 +0900

    xterm auto screen resize

--- src/components/console/xterm.rs
+++ src/components/console/xterm.rs
@@ -52,11 +52,40 @@ pub fn Xterm() -> Element {
 
             term_instance.set(Some(term));
 
-            // Call fit after a small delay to ensure rendering
+            // Initial fit
+            let fit_initial: XtermFitAddon = fit_addon.clone().unchecked_into();
             gloo_timers::callback::Timeout::new(100, move || {
-                fit_addon.fit();
+                fit_initial.fit();
             })
             .forget();
+
+            // --- Debounced Resize Handler ---
+            let win_resize = window().unwrap();
+            let fit_for_resize = fit_addon.clone();
+
+            // Shared state for debouncing
+            let timeout_handle = std::rc::Rc::new(std::cell::Cell::new(None));
+
+            let closure = wasm_bindgen::prelude::Closure::wrap(Box::new(move || {
+                let fit: XtermFitAddon = fit_for_resize.clone().unchecked_into();
+                let handle_cell = timeout_handle.clone();
+
+                // Cancel previous timeout
+                if let Some(h) = handle_cell.take() {
+                    drop(h);
+                }
+
+                // Set debounce (150ms)
+                let new_handle = gloo_timers::callback::Timeout::new(150, move || {
+                    fit.fit();
+                });
+                handle_cell.set(Some(new_handle));
+            }) as Box<dyn FnMut()>);
+
+            win_resize
+                .add_event_listener_with_callback("resize", closure.as_ref().unchecked_ref())
+                .unwrap();
+            closure.forget(); // Leak for the duration of the app for simplicity in this demo
         }
     });
 
